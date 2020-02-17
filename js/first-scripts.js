838738de1099a57325e0276913bf0b84127f54b7
Ned Faulhaber <nedfaulhaber@gmail.com>
2020-02-16 16:49:00 -0800

    limelight page set up

--- js/first-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("temp-mobile-nav").style.width = "250px";
-    document.getElementById("temp-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("temp-mobile-nav").style.width = "0";
-    document.getElementById("temp-wrapper").style.marginRight = "0";
-}
---
+++ js/tempy-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("tempy-mobile-nav").style.width = "250px";
+    document.getElementById("tempy-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("tempy-mobile-nav").style.width = "0";
+    document.getElementById("tempy-wrapper").style.marginRight = "0";
+}
