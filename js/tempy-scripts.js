d6c3d1b3da48b9179f4f6c76c53a500a2f18d511
Ned Faulhaber <nedfaulhaber@gmail.com>
2020-03-01 19:02:39 -0800

    finished skeleton of every page

--- js/tempy-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("tempy-mobile-nav").style.width = "250px";
-    document.getElementById("tempy-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("tempy-mobile-nav").style.width = "0";
-    document.getElementById("tempy-wrapper").style.marginRight = "0";
-}
